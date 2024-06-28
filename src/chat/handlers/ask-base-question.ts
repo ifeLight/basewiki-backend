3db3ccf444736f82fceec525235d3974296a2e6d
Ifedayo Karim <karimjnr@gmail.com>
2024-06-28 08:40:09 +0100

    fix error on chat error based on uncompleted history

--- src/chat/handlers/ask-base-question.ts
+++ src/chat/handlers/ask-base-question.ts
@@ -10,8 +10,8 @@ import { getCoinPriceTool } from "../tools/get-coin-price";
 export const askBaseQuestionHandler = async (request: IAskBaseQuestion): Promise<string> => {
     let history = await getChatHistory(request.sessionId)
     // if session greater than 5, remove the earliest history
-    if (history.length > 5) {
-        history = history.slice(-5)
+    if (history.length > 6) {
+        history = history.slice(-6)
     }
     const response = await generate({
         prompt: `You are acting as a helpful AI assistant that can answer questions related to the Base blockchain network. Do not answer questions related to other blockchain networks.
